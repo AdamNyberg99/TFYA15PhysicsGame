c39c94ba417c881c0cf00d388d80a6672f53cfac
AdamNyberg99 <adamnyberg1999@gmail.com>
2019-04-07 10:59:47 +0200

    Updated, now using the ParticleTwo class instead of Particle.

--- src/PhysicsCanvas.java
+++ src/PhysicsCanvas.java
@@ -1,9 +1,7 @@
-import java.awt.Canvas;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics2D;
+import java.awt.*;
 import java.awt.image.BufferStrategy;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 
@@ -11,10 +9,11 @@ import javax.swing.JFrame;
 public class PhysicsCanvas extends Canvas implements Runnable {
 	
 	private boolean running;
-	private Particle p1;
+	private ArrayList<ParticleTwo> planets = new ArrayList<>();
+	/*private Particle p1;
 	private Particle p2;
 	private Particle p3;
-	private Particle p4;
+	private Particle p4;*/
 
 
 	public PhysicsCanvas() {
@@ -22,11 +21,15 @@ public class PhysicsCanvas extends Canvas implements Runnable {
 		setPreferredSize(d);
 		setMinimumSize(d);
 		setMaximumSize(d);
-		
+
+		planets.add(new ParticleTwo(530,210,20, 120, 250,-32, Color.BLUE));
+		planets.add(new ParticleTwo(800,450,50,1000000*1000000, 2, -1, Color.YELLOW));
+		/*
 		p1 = new Particle(750, 450, 30, Color.RED);
 		p2 = new Particle(550, 350, 20, Color.BLUE);
 		p3 = new Particle(300, 800, 40, Color.GREEN);
 		p4 = new Particle(400, 200, 50, Color.YELLOW);
+		 */
 	}
 
 
@@ -61,21 +64,37 @@ public class PhysicsCanvas extends Canvas implements Runnable {
 		
 		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, getWidth(), getHeight());
-		
+
+		for(int i = 0; i < planets.size(); i++){
+			planets.get(i).render(g);
+		}
+		/*
 		p1.render(g);
 		p2.render(g);
 		p3.render(g);
 		p4.render(g);
+		 */
 				
 		strategy.show();
 	}
 
 
 	private void update() {
+		for(int i = 0; i < planets.size(); i++){
+			for (int j = 0; j < planets.size(); j++){
+
+				if(planets.get(i) != planets.get(j)){
+					planets.get(i).gravitation(planets.get(j));
+				}
+			}
+			planets.get(i).update();
+		}
+		/*
 		p1.update();
 		p2.update();
 		p3.update();
 		p4.update();
+		 */
 	}
 
 	public static void main(String[] args) {

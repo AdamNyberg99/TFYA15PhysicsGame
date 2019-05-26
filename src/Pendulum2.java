f9a2362654a6bf7ff10dc39ae1dff28492aa1a47
AdamNyberg99 <43615069+AdamNyberg99@users.noreply.github.com>
2019-05-26 15:41:39 +0200

    Update Pendulum2.java

--- src/Pendulum2.java
+++ src/Pendulum2.java
@@ -8,28 +8,31 @@ public class Pendulum2 extends ParticleTwo {
     private ArrayList<ParticleTwo> string = new ArrayList<>();
 
     public Pendulum2( double xanchor, double yanchor, Color color, double length) {
-
+        // INSTANTIATE VARIABLES
         this.angle = Math.PI / 3;
         this.xanchor = xanchor;
         this.yanchor = yanchor;
         this.dt = 0.06;
         this.angleVelocity = 0;
-
-        this.curPos = new Vec2D(xanchor + (int) (Math.sin(angle) * length), yanchor + (int) (Math.cos(angle) * length));
-        this.vel = new Vec2D(0, 0);
-        this.prevPos = curPos.sub(vel.mul(dt));
-
-        this.acel = new Vec2D(0,0);
-        this.mass = 10000;
+         this.mass = 10000;
 
         this.r = 45;
         this.color = color;
 
         this.length = length;
 
+        // INSTANTIATE NECESSARY VECTORS
+        this.curPos = new Vec2D(xanchor + (int) (Math.sin(angle) * length), yanchor + (int) (Math.cos(angle) * length));
+        this.vel = new Vec2D(0, 0);
+        this.prevPos = curPos.sub(vel.mul(dt));
+
+        this.acel = new Vec2D(0,0);
+       
+        // GENERATES A ROPE CONNECTING THE PENDELUM BALL TO THE ANCHOR_POINT
         generateString();
     }
 
+    // GENERATS A STRING/ROPE WITH THE LENGTH OF THE VARIABLE length, THE ROPE CONCISTS OF PARTICLE OBJECTS
     public void generateString(){
         for(int i = 0; i < length; i++){
             string.add(new ParticleTwo(xanchor,i,1,10,0,0,Color.RED));
@@ -41,16 +44,22 @@ public class Pendulum2 extends ParticleTwo {
     public void calcNewPos() {
         calcFnet();
         acel = F_net.div(mass);
-        //angleAccel = -5000 / length * Math.sin(angle);
+        
+        // UPDATE ANGLE ACCELERATION        
         angleAccel = -acel.getY() / length * Math.sin(angle);
+        // UPDATE ANGLE VELOCITY
         angleVelocity += angleAccel * dt;
+        // UPDATE ANGLE
         angle += angleVelocity * dt;
+        // UPDATE X AND Y COORDINATES OF THE PENDULUM
         curPos.setX(xanchor + (int) (Math.sin(angle) * length));
         curPos.setY(yanchor + (int) (Math.cos(angle) * length));
-
+        
+        // ROTATES THE ROPE CONECTING THE BALLA ANC ANCHOR-POINT
        rotateString(angleVelocity*dt);
     }
 
+    // DETECTS COLLISION WITH PARTICLE P1 AND THE PENDELUMS ROPE
     public boolean stringColission(ParticleTwo p1){
         for(ParticleTwo p2: string){
             Vec2D dPos = p2.curPos.sub(p1.curPos);
@@ -64,6 +73,8 @@ public class Pendulum2 extends ParticleTwo {
         return false;
     }
 
+    // MOVES THE ROPE TO ORIGO, AND THEN ROTATES IT AROUND ORIGO WITH THE GIVEN ANGLE toRotate, THEN MOVES IT BACK TO ITS
+    // ORIGINAL POSITION
     public void rotateString(double toRotate){
         for(ParticleTwo p: string){
             double tempX = p.curPos.getX() - xanchor;
@@ -77,22 +88,26 @@ public class Pendulum2 extends ParticleTwo {
             p.curPos = new Vec2D(rotatedX+xanchor,rotatedY+yanchor);
         }
     }
+    
+    // RENDERS EVERY PARTICLE IN THE ROPE/STRING
     public void renderString(Graphics2D g){
         for(ParticleTwo p: string){
             p.render(g);
         }
     }
 
+
     public void update(){
         calcNewPos();
-        //System.out.println("X = " + curPos.getX() + " ,y = "+ curPos.getY());
     }
 
     @Override
     public void render(Graphics2D g) {
+        // SET THE COLOR OF THE PENDULUM
         g.setColor(color);
+        // DRAW THE BIG CIRCLE AT THE END OF THE PENDULUM
         g.fillOval((int)Math.round(curPos.getX()-r), (int)Math.round(curPos.getY()-r), (int)Math.round(2*r), (int)Math.round(2*r));
-        //g.drawLine((int)xanchor, (int)yanchor, (int)curPos.getX(), (int)curPos.getY());
+        // RENDERS THE ROPE
         renderString(g);
     }
 }
